3d1c563b56bb719bc64ab0f0178e90aad69a0ccd
Michael Bieber <projects@traits.de>
2014-04-11 10:44:27 +0200

    CMake: variable names

--- src/protean3d/gl_layer.h
+++ src/protean3d/gl_layer.h
@@ -4,13 +4,13 @@
 //extern "C" {
 //#endif 
 
-#if PROTEAN3D_GL_IS_GL
+#if PROTEAN3D_GL_IS_OPENGL
   #if PROTEAN3D_GL_VERSION_MAJOR >= 4
     #include <glloadgen/GL_4_2/gl_core_4_2.h>
   #else
     #include <glloadgen/GL_3_3/gl_core_3_3.h>
   #endif
-#else if PROTEAN3D_GL_IS_ES
+#else if PROTEAN3D_GL_IS_OPENGL_ES
   // ES header here
 #endif
 
