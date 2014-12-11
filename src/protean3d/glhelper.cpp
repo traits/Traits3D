0ca7a7fcfe0169802fbe97ee12139a5679961105
Michael Bieber <os@traits.de>
2014-12-11 22:08:56 +0100

    Scaling helper: double -> GLfloat

---
+++ src/protean3d/glhelper.cpp
@@ -0,0 +1,36 @@
+#include "glhelper.h"
+
+GLfloat Protean3D::GL::excess(std::vector<glm::dvec3> const& val)
+{
+  GLfloat ret = 0;
+  for (auto v : val)
+  {
+    ret = std::max(ret, excess(v));
+  }
+  return ret;
+}
+
+
+std::vector<glm::vec3> Protean3D::GL::scale(double& exc, std::vector<glm::dvec3> const& val)
+{
+  std::vector<glm::vec3> ret(val.size());
+  exc = excess(val);
+
+  if (exc > 1)
+  {
+    for (auto i = 0; i != val.size(); ++i)
+    {
+      ret[i] = (val[i] / exc);
+    }
+  }
+  else
+  {
+    for (auto i = 0; i != val.size(); ++i)
+    {
+      ret[i].x = static_cast<GLfloat>(val[i].x);
+      ret[i].y = static_cast<GLfloat>(val[i].y);
+      ret[i].z = static_cast<GLfloat>(val[i].z);
+    }
+  }
+  return ret;
+}
--- src/protean3d/glhelper.h
+++ src/protean3d/glhelper.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
 #if defined PROTEAN3D_GL_IS_OPENGL
   #include <glloadgen/GL_3_3/gl_core_3_3.h>
 #elif defined PROTEAN3D_GL_IS_OPENGL_ES
@@ -9,3 +12,29 @@
 #endif
 
 #include "glm/glm.hpp"
+
+namespace Protean3D
+{
+  namespace GL
+  {
+    /**    
+     Checks, if the argument fits in a GLfloat by returning the scaling factor
+     associated with values position in (+/-)[0 ... max(GLfloat) ... max(double)]
+     \return m, with (+/-)m*max(GLfloat) == value (>1 for values, exceeding the range of GLfloat)  
+     */
+    inline GLfloat excess(double value)
+    {
+      return (value > 0)
+        ? static_cast<GLfloat>(value / std::numeric_limits<GLfloat>::max())
+        : -static_cast<GLfloat>(value / std::numeric_limits<GLfloat>::max());
+    }
+
+    inline GLfloat excess(glm::dvec3 value)
+    {
+      return std::max({ excess(value.x), excess(value.y), excess(value.z) });
+    }
+    
+    GLfloat excess(std::vector<glm::dvec3> const& val); 
+    std::vector<glm::vec3> scale(double& exc, std::vector<glm::dvec3> const& val);
+  }
+}
