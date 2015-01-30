83cb35e5fb46b91a01b753a04f10599aeed0390c
Michael Bieber <os@traits.de>
2015-01-30 22:55:17 +0100

    TextEngine: missing file added

---
+++ src/textengine/textengine.cpp
@@ -0,0 +1,20 @@
+#include <numeric>
+#include "protean3d/textengine/textengine.h"
+
+Protean3D::TextEngine::Hull::Hull()
+  : bl(std::numeric_limits<float>::max(), std::numeric_limits<float>::max())
+  , tr(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max())
+{
+}
+
+bool Protean3D::TextEngine::setText(std::string const& text)
+{
+  std::vector<std::string> textv(1, text);
+  return this->setText(textv);
+}
+
+std::string Protean3D::TextEngine::d2t(double val)
+{
+  return std::to_string(val);
+}
+
