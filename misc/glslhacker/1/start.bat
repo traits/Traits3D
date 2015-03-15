7c16fd3753ce6db0bc3c677255b0e5d453486657
Michael Bieber <os@traits.de>
2015-03-15 14:42:04 +0100

    glslhacker stuff removed

--- misc/glslhacker/1/start.bat
+++
@@ -1,2 +0,0 @@
-rem All command line parameters: http://www.geeks3d.com/forums/index.php/topic,3066.0.html
-start GLSLHacker.exe /demofile="mesh.xml"
--- misc/glslhacker/1/update.lua
+++
@@ -1,5 +0,0 @@
-gh_camera.bind(camera)
-gh_renderer.clear_color_depth_buffers(0.2, 0.2, 0.2, 1.0, 1.0)
-
-gh_gpu_program.bind(color_prog)
-gh_object.render(grid)
