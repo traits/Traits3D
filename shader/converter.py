189e1967d57797d0e2931055dceb6cbfa0526625
Michael Bieber <os@traits.de>
2015-05-28 00:29:51 +0200

    Python: Moved code into Converter class

--- shader/converter.py
+++ shader/converter.py
@@ -1,36 +1,52 @@
 import os
 from string import Template
 
-# config section
+class Converter(object):
+  """Converts template shader files into C++ sources"""
 
-namespace = ['Traits3D', 'GL', 'ShaderCode']
+  def __init__(self):
+    self.__vertex_input_dir = 'input' + os.sep + 'vshader'
+    self.__fragment_input_dir = 'input' + os.sep + 'fshader'
+    self.__function_input_dir = 'input' + os.sep + 'functions'
+    self.__hout = '..' + os.sep + 'src' + os.sep + 'glbase' + os.sep + 'include'  + os.sep + 'traits3d' + os.sep + 'glbase'
+    self.__cppout = '..' + os.sep + 'src' + os.sep + 'glbase'
+    self.__insert = ''
+
+  def openFiles(self):
+    self.__hfile = open(os.path.join(self.__hout, "shader_std.h"),'w')
+    self.__cppfile = open(os.path.join(self.__cppout, "shader_std.cpp"),'w')
+  
+  def closeFiles(self):
+    self.__hfile.close()
+    self.__cppfile .close()
 
 
-# The key is used as a variable (e.g. $proj_matrix) in input shader templates
-# and will be transformed to value in ouput files (C++ or shader). 
-# The key should remain consistent with the input shader content
+  # static config section
 
-shader_variables = { 
-  'proj_matrix'   : 'proj_matrix',
-  'mv_matrix'     : 'mv_matrix',
-  'v_in_color'    : 'v_in_color', 
-  'v_coordinates' : 'v_coordinates',
-  'v_normals'     : 'v_normals',  
-  'v_out_color'   : 'v_out_color',  
-  }
+  __touch_warning = '// generated file - don\'t touch\n\n'
+  __namespace = ['Traits3D', 'GL', 'ShaderCode']
 
-#def decorate(variables):
-#  surround = lambda word : '$' + word 
-#  return dict(zip(map(surround, variables.keys()), variables.values()))
 
-#shader_variables = decorate(shader_variables)
+  # The key is used as a variable (e.g. $proj_matrix) in input shader templates
+  # and will be transformed to value in ouput files (C++ or shader). 
+  # The key should remain consistent with the input shader content
 
-version_gl = {
-  'es'      : '#version 300 es',
-  'desktop' : '#version 330',
-  }
+  __shader_variables = { 
+    'proj_matrix'   : 'proj_matrix',
+    'mv_matrix'     : 'mv_matrix',
+    'v_in_color'    : 'v_in_color', 
+    'v_coordinates' : 'v_coordinates',
+    'v_normals'     : 'v_normals',  
+    'v_out_color'   : 'v_out_color',  
+    }
 
-cpp_version_selector = \
+  #def decorate(variables):
+  #  surround = lambda word : '$' + word 
+  #  return dict(zip(map(surround, variables.keys()), variables.values()))
+
+  #__shader_variables = decorate(__shader_variables)
+
+  __cpp_version_selector = \
 '''
 #ifdef GL_ES_VERSION_3_0
   "#version 300 es\\n"
@@ -39,117 +55,91 @@ cpp_version_selector = \
 #endif
 '''
 
-variables_struct = 'Var'
-vertex_class = 'Vertex'
-fragment_class = 'Fragment'
-function_class = 'Function'
+  __variables_struct = 'Var'
+  __vertex_class = 'Vertex'
+  __fragment_class = 'Fragment'
+  __function_class = 'Function'
 
+  __h_rel_path = 'traits3d/glbase'
+  __h_preamble = \
+    '#pragma once\n\n' \
+    + __touch_warning \
+    +'#include <string>\n\n'
 
-vertex_input_dir = 'input' + os.sep + 'vshader'
-fragment_input_dir = 'input' + os.sep + 'fshader'
-function_input_dir = 'input' + os.sep + 'functions'
-hout = '..' + os.sep + 'src' + os.sep + 'glbase' + os.sep + 'include'  + os.sep + 'traits3d' + os.sep + 'glbase'
-cppout = '..' + os.sep + 'src' + os.sep + 'glbase'
-h_rel_path = 'traits3d/glbase'
+  # end config
 
-hfile = open(os.path.join(hout, "shader_std.h"),'w')
-cppfile = open(os.path.join(cppout, "shader_std.cpp"),'w')
+  def _intend(self) :
+    self.__insert += 2 * ' '
 
-warning = '// generated file - don\'t touch\n\n'
+  def _unintend(self) :
+    self.__insert = self.__insert.replace(' ', '', 2)
 
-h_preamble = \
-  '#pragma once\n\n' \
-  + warning \
-  +'#include <string>\n\n'
-
-# end config
-
-insert = ''
-def intend() :
-  global insert
-  insert += 2 * ' '
-
-def unintend() :
-  global insert
-  insert = insert.replace(' ', '', 2)
-
-hfile.write(h_preamble)
-
-for n in namespace:
-  hfile.write(insert + 'namespace ' + n + '\n' + insert + '{\n')
-  intend()
-
-hfile.write(insert + '// shader variables\n')
-hfile.write(insert + 'struct ' + variables_struct + '\n' + insert + '{\n')
-intend()
-for v in sorted(shader_variables.itervalues()):
-  hfile.write(insert + 'static const std::string ' + v + ';\n')
-unintend()
-hfile.write(insert + '};\n\n')
-
-cppfile.write(warning + '#include "' + h_rel_path + "/" + 'shader_std.h"\n\n')
-for v in sorted(shader_variables.itervalues()):
-  cppfile.write('const std::string ' + '::'.join(namespace) + '::' + variables_struct + '::' + v + ' = "' + v + '";\n')
-
-basenames = []
-caption = []
-
-
-def processFiles(dir, type):
-  ret = {}
-  for root, dirs, files in os.walk(dir):
-    for fname in sorted(files):
-      ifile = os.path.join(root, fname)     
-      basename = os.path.splitext(os.path.basename(ifile))[0]
+  @staticmethod
+  def processFiles(dir, type):
+    ret = {}
+    for root, dirs, files in os.walk(dir):
+      for fname in sorted(files):
+        ifile = os.path.join(root, fname)     
+        basename = os.path.splitext(os.path.basename(ifile))[0]
     
-      with open(ifile, mode='rb') as file:     
-        text = [] 
-        for line in file:
-          #eols = line.count('\n')
-          line = line.rstrip('\r\n')
-          line = Template(line).substitute(shader_variables)
-          text.append(line)
-      ret[basename] = [fname, type, text]   
-  return ret
+        with open(ifile, mode='rb') as file:     
+          text = [] 
+          for line in file:
+            #eols = line.count('\n')
+            line = line.rstrip('\r\n')
+            line = Template(line).substitute(Converter.__shader_variables)
+            text.append(line)
+        ret[basename] = [fname, type, text]   
+    return ret
 
-def writeOutput(comment, artifact_class, artifact_input_dir):
-  a_info = processFiles(artifact_input_dir, artifact_class)
-  hfile.write(insert + comment + '\n')
-  hfile.write(insert + 'class ' + artifact_class + '\n' + insert + '{\n')
-  hfile.write(insert + 'public:\n')
-  intend()
-  cppfile.write('\n\n' + comment + '\n')
-  for k, v in sorted(a_info.iteritems()):
-    hfile.write(insert + 'static const char* ' + k + ';\n')
-    cppfile.write('\n\n// ' + v[0] + '\n') 
-    cppfile.write('const char* ' + '::'.join(namespace) + '::' + artifact_class + '::' + k + ' = \n{')
-    cppfile.write(cpp_version_selector)
-    for line in v[2]:
-      if line and not line.isspace():
-        #if line.startswith('//'):
-        #  cppfile.write(line + '\n')
-        #else:
-        cppfile.write('  "' + line + '\\n"' + '\n')
-    cppfile.write('};\n\n')
-  unintend()
-  hfile.write(insert + '};\n\n')
-  return
+  def writeOutput(self, comment, artifact_class, artifact_input_dir):
+    a_info = Converter.processFiles(artifact_input_dir, artifact_class)
+    self.__hfile.write(self.__insert + comment + '\n')
+    self.__hfile.write(self.__insert + 'class ' + artifact_class + '\n' + self.__insert + '{\n')
+    self.__hfile.write(self.__insert + 'public:\n')
+    self._intend()
+    self.__cppfile.write('\n\n' + comment + '\n')
+    for k, v in sorted(a_info.iteritems()):
+      self.__hfile.write(self.__insert + 'static const char* ' + k + ';\n')
+      self.__cppfile.write('\n\n// ' + v[0] + '\n') 
+      self.__cppfile.write('const char* ' + '::'.join(Converter.__namespace) + '::' + artifact_class + '::' + k + ' = \n{')
+      self.__cppfile.write(Converter.__cpp_version_selector)
+      for line in v[2]:
+        if line and not line.isspace():
+          #if line.startswith('//'):
+          #  self.__cppfile.write(line + '\n')
+          #else:
+          self.__cppfile.write('  "' + line + '\\n"' + '\n')
+      self.__cppfile.write('};\n\n')
+    self._unintend()
+    self.__hfile.write(self.__insert + '};\n\n')
+    return
 
-writeOutput(r'// Vertex shader', vertex_class, vertex_input_dir)
-writeOutput(r'// Fragment shader', fragment_class, fragment_input_dir)
-#writeOutput(r'// Functions', function_class, function_input_dir)
+  def run(self):
+    self.__hfile.write(Converter.__h_preamble)
 
+    for n in Converter.__namespace:
+      self.__hfile.write(self.__insert + 'namespace ' + n + '\n' + self.__insert + '{\n')
+      self._intend()
 
-#s = '''    }
-#  };
-#} // ns  
-#'''    
-#hfile.write(s)
+    self.__hfile.write(self.__insert + '// shader variables\n')
+    self.__hfile.write(self.__insert + 'struct ' + Converter.__variables_struct + '\n' + self.__insert + '{\n')
+    self._intend()
+    for v in sorted(Converter.__shader_variables.itervalues()):
+      self.__hfile.write(self.__insert + 'static const std::string ' + v + ';\n')
+    self._unintend()
+    self.__hfile.write(self.__insert + '};\n\n')
 
-insert = insert.replace(' ', '', 2)
-for n in reversed(namespace):
-  hfile.write(insert + '} // ' + n + '\n')
-  unintend()
+    self.__cppfile.write(Converter.__touch_warning + '#include "' + Converter.__h_rel_path + "/" + 'shader_std.h"\n\n')
+    for v in sorted(Converter.__shader_variables.itervalues()):
+      self.__cppfile.write('const std::string ' + '::'.join(Converter.__namespace) + '::' + Converter.__variables_struct + '::' + v + ' = "' + v + '";\n')
 
-hfile.close()  
-cppfile.close()    
+    self.writeOutput(r'// Vertex shader', Converter.__vertex_class, self.__vertex_input_dir)
+    self.writeOutput(r'// Fragment shader', Converter.__fragment_class, self.__fragment_input_dir)
+    #self.writeOutput(r'// Functions', Converter.__function_class, self.__function_input_dir)
+
+    self.__insert = self.__insert.replace(' ', '', 2)
+    for n in reversed(Converter.__namespace):
+      self.__hfile.write(self.__insert + '} // ' + n + '\n')
+      self._unintend()
+    return
---
+++ shader/main.py
@@ -0,0 +1,6 @@
+from converter import Converter
+
+conv = Converter()
+conv.openFiles()
+conv.run()
+conv.closeFiles()
